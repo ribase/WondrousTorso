5e0c94b825a451b72160c4f348737a7f00d9ad61
Sebastian Thadewald <sebastian.thadewald@cabag.ch>
2017-07-05 13:18:15 +0200

    init

---
+++ ext_emconf.php
@@ -0,0 +1,54 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "Wondrous.Wondrous_torso".
+ *
+ * Auto generated 03-10-2016 11:05
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Torso Element',
+	'description' => '',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '1.0.2',
+	'dependencies' => 'cms,extbase,fluid,flux,fluidcontent,vhs',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author' => 'Sebastian Thadewald',
+	'author_email' => 'sebastian@wondrous.ch',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5-7.6.99',
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+			'flux' => '',
+			
+			'fluidcontent' => '',
+			'vhs' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+	'suggests' => array(
+	),
+);
